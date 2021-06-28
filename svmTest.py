def3a20b467a8a086379840ebcf23abaf818ab29
fproks <fproks@126.com>
2021-06-28 20:03:45 +0800

    添加SVM对比实验

---
+++ svmTest.py
@@ -0,0 +1,21 @@
+import numpy as np
+import scipy.io as sio
+from  sklearn.preprocessing import  LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from coTraining import COTraining
+_tmp = sio.loadmat("./data_set/vggdata.mat")
+data = _tmp["data"]
+stand = _tmp["target"].flatten()
+stand=LabelEncoder().fit_transform(stand)
+train_x,test_x,train_y,test_y=train_test_split(data,stand,test_size=0.8)
+
+svm=SVC()
+svm.fit(train_x,train_y)
+print(svm.score(test_x,test_y))
+
+
+
+
+
+
