7fec7950b4047a2d6882993e9938e19bd0c3592c
fproks <fproks@126.com>
2021-06-29 20:14:12 +0800

    修复了一些BUG,添加log

--- coTraining.py
+++ coTraining.py
@@ -3,11 +3,12 @@ from sklearn.svm import SVC
 import numpy as np
 import operator
 from Pretreatment import Pretreatment
-
-
+import time
+from config import  LOGGER
 
 class COTraining(object):
-    def __init__(self, train_x: np.ndarray, train_y: np.ndarray, iter_x: np.ndarray, iter_y: np.ndarray,RSMSize=0.9):
+    def __init__(self, train_x: np.ndarray, train_y: np.ndarray, iter_x: np.ndarray, iter_y: np.ndarray, RSMSize=0.9,
+                 bvsbFilter=0.7, iterPrecent=0.1):
 
         assert train_x.ndim == 2
         assert train_y.ndim == 1
@@ -16,38 +17,42 @@ class COTraining(object):
         assert train_x.shape[0] == train_y.shape[0]
         assert iter_x.shape[0] == iter_y.shape[0]
         assert train_x.shape[1] == iter_x.shape[1]
-        self.originDataDim=train_x.shape[1]
+        self.originDataDim = train_x.shape[1]
         self.firstClassifier = SVC(probability=True)
         self.secondClassifier = SVC(probability=True)
         self.first_train_x = np.copy(train_x)
-        self.second_train_x=np.copy(train_x)
+        self.second_train_x = np.copy(train_x)
         self.train_y = train_y
         self.first_iter_x = np.copy(iter_x)
-        self.second_iter_x=np.copy(iter_x)
+        self.second_iter_x = np.copy(iter_x)
         self.iter_y = iter_y
         self.isIter = True
-        self.singleIterMaxLength = len(self.iter_y) * 0.05
-        self.allIterMaxLength = len(self.iter_y) * 0.5
-        assert  RSMSize>0
-        assert RSMSize<self.originDataDim
-        if RSMSize<1:
-            self.RSMSize=int(train_x.shape[1]*RSMSize)
+        self.singleIterMaxLength = int(len(self.iter_y) * iterPrecent)
+        self.allIterMaxLength = int(len(self.iter_y) * 0.5)
+        self.bvsbFilter = bvsbFilter
+        assert RSMSize > 0
+        assert RSMSize < self.originDataDim
+        if RSMSize < 1:
+            self.RSMSize = int(train_x.shape[1] * RSMSize)
         else:
-            self.RSMSize=int(RSMSize)
-        self.firstRSMIndex=np.array(range(self.originDataDim))
-        self.secondRSMIndex=np.array(range(self.originDataDim))
+            self.RSMSize = int(RSMSize)
+        self.firstRSMIndex = np.array(range(self.originDataDim))
+        self.secondRSMIndex = np.array(range(self.originDataDim))
 
     def createRSM(self):
-        self.firstRSMIndex=Pretreatment.createRSMIndex(self.originDataDim,self.RSMSize)
-        self.secondRSMIndex=Pretreatment.createRSMIndex(self.originDataDim,self.RSMSize)
-        self.first_train_x=self.first_train_x[:,self.firstRSMIndex]
-        self.second_train_x=self.second_train_x[:,self.secondRSMIndex]
-        self.first_iter_x=self.first_iter_x[:,self.firstRSMIndex]
-        self.second_iter_x=self.second_iter_x[:,self.secondRSMIndex]
+        self.firstRSMIndex = Pretreatment.createRSMIndex(self.originDataDim, self.RSMSize)
+        self.secondRSMIndex = Pretreatment.createRSMIndex(self.originDataDim, self.RSMSize)
+        self.first_train_x = self.first_train_x[:, self.firstRSMIndex]
+        self.second_train_x = self.second_train_x[:, self.secondRSMIndex]
+        self.first_iter_x = self.first_iter_x[:, self.firstRSMIndex]
+        self.second_iter_x = self.second_iter_x[:, self.secondRSMIndex]
 
     def fit(self, test_x, test_y):
+        print("------------------------TRAIN----------------------------------")
+        i = 1
         while self.isIter:
-            print("------------------------TRAIN----------------------------------")
+            start = time.time_ns()
+            print(f'当前训练集合大小为{len(self.train_y)}')
             self.firstClassifier.fit(self.first_train_x, self.train_y)
             self.secondClassifier.fit(self.second_train_x, self.train_y)
             proba1 = self.firstClassifier.predict_proba(self.first_iter_x)
@@ -58,7 +63,11 @@ class COTraining(object):
             if len(self.iter_y) < self.allIterMaxLength:
                 self.isIter = False
             self.resetTrainAndIterData(iterIndex)
-            print(self.score(test_x, test_y))
+            print(f'第{i}次训练结束，共获取可迭代数据{len(iterIndex)}个')
+            print(f'第{i}次训练结束,测试结果为{self.score(test_x, test_y):0.6f}')
+            end = time.time_ns()
+            print(f'本次训练共用时{(end - start) / 1000 / 1000 / 1000:0.2f}秒')
+            i = i + 1
 
     '''
         获取类别相同的数据的索引
@@ -82,34 +91,60 @@ class COTraining(object):
         for i in range(len(proba1)):
             if _bvsb[i] > filter:
                 resIndex.append(i)
+        LOGGER.debug(f'满足bvsb间隔大于{filter}的个数为{len(resIndex)}个')
         maxSize = int(min(self.singleIterMaxLength * 2, len(resIndex)) * -1)
-        #满足的值的前maxSize个索引
+        # 满足的值的前maxSize个索引
         index = np.argsort(_bvsb[resIndex])[maxSize:]
-        #最终的索引
+        LOGGER.debug(f'根据条件共需要选出前{maxSize*-1}个')
+        # 最终的索引
         return np.array(resIndex)[index]
 
     # 获取下一次迭代需要添加的数据的索引
     def getIterDataIndex(self, proba1, proba2):
         sameIndex = self._getSameCategoryIndex(proba1, proba2)
-        _bvsb1_index = self._bvsbIndex(proba1[sameIndex])
-        _bvsb2_index = self._bvsbIndex(proba2[sameIndex])
-        tmp = np.argwhere(_bvsb2_index == _bvsb1_index).flatten().astype(int)
-        data_index = sameIndex[_bvsb1_index[tmp]]
+        LOGGER.debug(f'共获取标签相同元素{len(sameIndex)}个')
+        _bvsb1_index = self._bvsbIndex(proba1[sameIndex], self.bvsbFilter)
+        _bvsb2_index = self._bvsbIndex(proba2[sameIndex], self.bvsbFilter)
+        #tmp = np.argwhere(_bvsb2_index == _bvsb1_index).flatten().astype(int)
+        #LOGGER.debug(f'均满足条件的数据的个数有{len(tmp)}个')
+        #data_index = sameIndex[_bvsb1_index[tmp]]
+        id=np.intersect1d(_bvsb1_index,_bvsb2_index)
+        data_index=sameIndex[id]
+        LOGGER.debug(f'均满足条件的数据个数有{len(id)}个')
         return data_index
 
     # 将下一次需要迭代训练的数据添加到训练集中
     def resetTrainAndIterData(self, iterDataIndex):
         prepareY = self.iter_y[iterDataIndex]
         self.first_train_x = numpy.append(self.first_train_x, self.first_iter_x[iterDataIndex], axis=0)
-        self.second_train_x=numpy.append(self.second_train_x,self.second_iter_x[iterDataIndex],axis=0)
+        self.second_train_x = numpy.append(self.second_train_x, self.second_iter_x[iterDataIndex], axis=0)
         self.train_y = numpy.append(self.train_y, prepareY, axis=0)
         self.first_iter_x = np.delete(self.first_iter_x, iterDataIndex, axis=0)
-        self.second_iter_x=np.delete(self.second_iter_x,iterDataIndex,axis=0)
+        self.second_iter_x = np.delete(self.second_iter_x, iterDataIndex, axis=0)
         self.iter_y = np.delete(self.iter_y, iterDataIndex, axis=0)
+        print(f'剩余训练数据个数为{len(self.iter_y)}')
 
-    def predict(self, test_x):
-        first_test_x=test_x[:,self.firstRSMIndex]
-        second_test_x=test_x[:,self.secondRSMIndex]
+    def predict(self,test_X):
+        first_test_x = test_X[:, self.firstRSMIndex]
+        second_test_x = test_X[:, self.secondRSMIndex]
+        proba1 = self.firstClassifier.predict_proba(first_test_x)
+        proba2 = self.secondClassifier.predict_proba(second_test_x)
+        result=np.zeros(test_X.shape[0],dtype=np.int)
+        first_max_p=np.max(proba1,axis=1)
+        first_max_i=np.argmax(proba1,axis=1)
+        second_max_p=np.max(proba2,axis=1)
+        second_max_i=np.argmax(proba2,axis=1)
+        for i in range(test_X.shape[0]):
+            if first_max_p[i]>second_max_p[i]:
+                result[i]=first_max_i[i]
+            else:
+                result[i]=second_max_i[i]
+        return  result
+
+
+    def predict1(self, test_x):
+        first_test_x = test_x[:, self.firstRSMIndex]
+        second_test_x = test_x[:, self.secondRSMIndex]
         proba1 = self.firstClassifier.predict_proba(first_test_x)
         proba2 = self.secondClassifier.predict_proba(second_test_x)
         res1 = np.argmax(proba1, axis=1)
@@ -119,13 +154,13 @@ class COTraining(object):
         result[sameIndex] = res1[sameIndex]
         if len(sameIndex) < test_x.shape[0]:
             notSameIndex = np.delete(np.arange(len(res1)), sameIndex)
-            first_max_p=np.sort(proba1,axis=1)[notSameIndex,-1]
-            first_max_i=np.argsort(proba1,axis=1)[notSameIndex,-1]
-            second_max_p=np.sort(proba2,axis=1)[notSameIndex,-1]
-            second_max_i=np.sort(proba2,axis=1)[notSameIndex,-1]
-            notSameC=[]
+            first_max_p = np.max(proba1, axis=1)[notSameIndex]
+            first_max_i = np.argmax(proba1, axis=1)[notSameIndex]
+            second_max_p = np.max(proba2, axis=1)[notSameIndex]
+            second_max_i = np.argmax(proba2, axis=1)[notSameIndex]
+            notSameC = []
             for i in range(len(notSameIndex)):
-                if first_max_p[i]>second_max_p[i]:
+                if first_max_p[i] > second_max_p[i]:
                     notSameC.append(first_max_i[i])
                 else:
                     notSameC.append(second_max_i[i])
--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import numpy as np
 import scipy.io as sio
-from  sklearn.preprocessing import  LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from coTraining import COTraining
-_tmp = sio.loadmat("./data_set/vggdata.mat")
+from sklearn.decomposition import PCA
+
+_tmp = sio.loadmat("./data_set/cifar_vgg_data_l2.mat")
 data = _tmp["data"]
 stand = _tmp["target"].flatten()
-stand=LabelEncoder().fit_transform(stand)
-train_x,test_x,train_y,test_y=train_test_split(data,stand,test_size=0.8)
+stand = LabelEncoder().fit_transform(stand)
 
-final_test_x=np.copy(test_x)
-final_test_y=np.copy(test_y)
-from  collections import Counter
-
-ttt=Counter(stand)
-
-co=COTraining(train_x,train_y,test_x,test_y)
-co.fit(final_test_x,final_test_y)
-print(co.score(final_test_x,final_test_y))
+pca=PCA(n_components=300)
+data=pca.fit_transform(data)
 
 
+train_x, test_x, train_y, test_y = train_test_split(data, stand, test_size=0.95)
 
+final_test_x = np.copy(test_x)
+final_test_y = np.copy(test_y)
+from collections import Counter
 
+ttt = Counter(stand)
 
-
-
+co = COTraining(train_x, train_y, test_x, test_y,RSMSize=0.8,bvsbFilter=0.7,iterPrecent=0.2)
+co.fit(final_test_x, final_test_y)
+print(co.score(final_test_x, final_test_y))
