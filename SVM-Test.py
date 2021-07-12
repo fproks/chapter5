ff3af8de8c79c9918cf4c01cc0484302e0b23756
rdjeans <84707291+rdjeans@users.noreply.github.com>
2021-07-12 10:57:32 +0800

    特征提取和方法测试

--- SVM-Test.py
+++ SVM-Test.py
@@ -4,7 +4,7 @@ from  sklearn.preprocessing import  LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from coTraining import COTraining
-_tmp = sio.loadmat("./data_set/vggdata.mat")
+_tmp=sio.loadmat(r"E:\data_chapter5\cifar_vgg_data_l2.mat")
 data = _tmp["data"]
 stand = _tmp["target"].flatten()
 stand=LabelEncoder().fit_transform(stand)
@@ -13,6 +13,8 @@ train_x,test_x,train_y,test_y=train_test_split(data,stand,test_size=0.95,random_
 svm=SVC()
 svm.fit(train_x,train_y)
 print(svm.score(test_x,test_y))
+print("------------------------------------------------------------------------------------------")
+print("---------------------------------SVM------------------------------------------------------")
 
 
 
